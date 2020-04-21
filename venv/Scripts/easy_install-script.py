e730f40f88730cab51636281cb16a5113a209a55
jaarfi <day@arendt.cc>
2020-04-21 13:04:32 +0200

    Initial Commmit

--- venv/Scripts/easy_install-script.py
+++ venv/Scripts/easy_install-script.py
@@ -1,4 +1,4 @@
-#!"C:\Users\Jan Arendt\PycharmProjects\ProjektSpielerei\venv\Scripts\python.exe"
+#!"C:\Users\Jan Arendt\PycharmProjects\NiklasUndJansGeilesGame\venv\Scripts\python.exe"
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.1.0','console_scripts','easy_install'
 __requires__ = 'setuptools==39.1.0'
 import re
