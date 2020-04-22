0e9897a65e5b37168fd35bd87d2e010133fc7faa
jaarfi <day@arendt.cc>
2020-04-22 13:25:02 +0200

    "2" Spieler ( der eine Kann nichts auser rumstehen momentan), Lifetotals, Shelld fleigen in Parabel, bei hit Lebensabzug

--- Test1.py
+++ Test1.py
@@ -1,4 +1,5 @@
 import os, math, time, random, enum
+import numpy as np
 from os import listdir
 from os.path import isfile, join
 from shapely.geometry import Polygon
@@ -29,6 +30,23 @@ class shellStates(enum.Enum):
     FLYING = 2
     EXPLODING = 3
 
+class colors(enum.Enum):
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    red = (255, 0, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
+    cyan = (0, 255, 255)
+    khaki = (255, 246, 143)
+    grey = (127, 127, 127)
+
+    light_grey = (200, 200, 200)
+
+    dark_grey = (50, 50, 50)
+    dark_orange = (255, 140, 0)
+
+    springgreen = (0, 255, 127)
+
 class Tank(object):
 
     '''
@@ -42,6 +60,8 @@ class Tank(object):
         self.angle = 45
         self.shootingVector = (int(math.sin(-self.angle)*150), int(math.cos(-self.angle)*150))
         self.shell = Shell()
+        self.life = 100
+        self.color =  list(np.random.random(3) * 256)
 
     def move(self, dx, dy):
         # Move each axis separately. Note that this checks for collisions both times.
@@ -64,14 +84,20 @@ class Tank(object):
         if self.shell.state == shellStates.IDLE:
             self.shell.fireShell(self.shootingVector, self.getCoords())
 
-    def firingAnimation(self, map, frame):
-        self.shell.collisionCheck(map)
+    def firingAnimation(self, listOfObjects, frame):
+        listCopy = list.copy(listOfObjects)
+        listCopy.remove(self)
+        self.shell.collisionCheck(listCopy)
         if self.shell.state == shellStates.FLYING:
             pass
 
+    def hit(self):
+        self.life = self.life-20
+
     def draw(self, display):
-        print(self)
-        pg.draw.rect(display, (255, 200, 0), self.rect)
+        print(self,self.rect)
+        pg.draw.rect(display, self.color, self.rect)
+        pg.draw.line(display, self.color, (self.rect.x, self.rect.y-10), (self.rect.x+self.life, self.rect.y-10))
         self.shell.draw(display)
         if self.shell.state == shellStates.IDLE:
             pg.draw.line(display, (255, 50, 0), (self.rect.x, self.rect.y),(self.rect.x+self.shootingVector[0], self.rect.y+self.shootingVector[1]))
@@ -83,6 +109,7 @@ class Shell(object):
         self.directionVector = (0, 0)
         self.state = shellStates.IDLE
         self.explodeFrameCounter = 0
+        self.dy = -4
 
     def draw(self,display):
         self.move()
@@ -95,22 +122,27 @@ class Shell(object):
                 self.move((-1,-1))
                 self.explodeFrameCounter = 0
 
-
-    def move(self, coords=(0,0)):
+    def move(self, coords=(0, 0)):
         if self.state == shellStates.FLYING:
             if coords == (0,0):
                 self.rect.x = self.rect.x + int(self.directionVector[0]/10)
-                self.rect.y = self.rect.y + int(self.directionVector[1]/10)
+                self.rect.y = self.rect.y + int(self.directionVector[1]/10) + self.dy
                 self.polygon = [self.rect.bottomleft, self.rect.topleft, self.rect.topright, self.rect.bottomright]
+                self.dy = self.dy + 1
         elif coords != (0,0):
             self.rect.x = coords[0]
             self.rect.y = coords[1]
             self.polygon = [self.rect.bottomleft, self.rect.topleft, self.rect.topright, self.rect.bottomright]
 
         self.polygon = [self.rect.bottomleft, self.rect.topleft, self.rect.topright, self.rect.bottomright]
-    def collisionCheck(self,map):
-        if Polygon(self.polygon).intersects(Polygon(map.polygon)):
-            self.state=shellStates.EXPLODING
+
+    def collisionCheck(self, listOfObjects):
+        if self.state == shellStates.FLYING:
+            for collision in listOfObjects:
+                if Polygon(self.polygon).intersects(Polygon(collision.polygon)):
+                    self.dy = -4
+                    self.state=shellStates.EXPLODING#
+                    collision.hit()
 
     def fireShell(self, shootingVector, startpoint):
         self.directionVector = shootingVector
@@ -131,8 +163,13 @@ class Map(object):
     def draw(self,display):
         pg.gfxdraw.filled_polygon(display, map.polygon, (255, 255, 255))
 
+    def hit(self):
+        pass
 
 
+def drawAllObjects(arrayOfObjects, display):
+    for toDraw in arrayOfObjects:
+        toDraw.draw(display)
 
 def createMap(width,height):
   '''
@@ -210,7 +247,11 @@ players = []
 players.append(Tank())
 players[0].move(100,100)
 players.append(Tank())
-
+print(players[0]==players[1])
+listOfObjects = []
+listOfObjects.append(map)
+for player in players:
+    listOfObjects.append(player)
 frameCounterForAnimations = 0
 
 # Spielschleife
@@ -238,7 +279,7 @@ while True:
     players[1].move(0, 100)
     while Polygon(map.polygon).intersects(Polygon(players[0].polygon)):
         players[0].move(0, -1)
-    while Polygon(map.polygon).intersects(Polygon(players[0].polygon)):
+    while Polygon(map.polygon).intersects(Polygon(players[1].polygon)):
         players[1].move(0, -1)
 
     for event in pg.event.get():
@@ -247,12 +288,11 @@ while True:
             quit()
 
     display.fill((0, 0, 0))
-    map.draw(display)
-    players[0].draw(display)
-    players[1].draw(display)
+    drawAllObjects(listOfObjects, display)
+    print("\n")
 
-    players[0].firingAnimation(map, frameCounterForAnimations)
-    players[1].firingAnimation(map, frameCounterForAnimations)
+    players[0].firingAnimation(listOfObjects, frameCounterForAnimations)
+    players[1].firingAnimation(listOfObjects, frameCounterForAnimations)
     '''
     if angle == 359:
         angle = 0
