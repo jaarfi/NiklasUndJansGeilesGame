a7774c6cfd607a4f4f1fe4118df4c65809862543
niggi01 <56389368+niggi01@users.noreply.github.com>
2020-04-23 17:58:06 +0200

    draw rects

--- bla.py
+++ bla.py
@@ -1,21 +1,53 @@
-import pygame
+import pygame, math
 
 WIDTH = 500
 HEIGHT = 500
-FPS = 30
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
-bild = pygame.image.load("pics/tank/tank_model_1.png")
-s_bild = pygame.transform.scale(bild, (80, 45))
-# r_bild = pygame.transform.rotate(s_bild,rot)
 
-rec = pygame.Rect(200, 200, 80, 45)
+class Tank(object):
+    def __init__(self):
+        self.bild = pygame.image.load("pics/tank/tank_model_1.png")
+        self.s_bild = pygame.transform.scale(self.bild, (80, 45))
+        self.outer_rec = pygame.Rect(200, 200, self.s_bild.get_width(), self.s_bild.get_height())
+        self.map_rec = self.buildRecMap()
 
-rot = 0
-rot_speed = 2
+    def buildRecMap(self, a=0):
+        cos = abs(math.cos(math.radians(a)))+0.00001
+        sin = abs(math.sin(math.radians(a)))+0.00001
+
+        rec2width = ((self.outer_rec.topright[0] - self.outer_rec.topleft[0])/2) * sin
+        rec2height = (self.outer_rec.bottomleft[1] - self.outer_rec.topleft[1]) * cos
+        r = pygame.Rect(self.outer_rec.center[0] - rec2width / 2, self.outer_rec.center[1] - rec2height / 2,
+                        rec2width, rec2height)
+        return r
+
+    def draw(self, rt):
+        r_bild = pygame.transform.rotate(player.s_bild, rt)
+
+        pos = (self.outer_rec.center[0] - (r_bild.get_width() / 2), self.outer_rec.center[1] - (r_bild.get_height() / 2))
+        screen.blit(r_bild, pos)
+
+        self.map_rec = self.buildRecMap(rt)
+        drawrec(self.outer_rec)
+        drawrec(self.map_rec, (255, 128, 0), 2)
+
+
+def drawrec(rec, c=(255, 0, 0), i=3):
+    pygame.draw.circle(screen, c, rec.center, i)
+    pygame.draw.circle(screen, c, rec.topleft, i)
+    pygame.draw.circle(screen, c, rec.topright, i)
+    pygame.draw.circle(screen, c, rec.bottomleft, i)
+    pygame.draw.circle(screen, c, rec.bottomright, i)
+
+
+rt = 0
+rot_speed = 1
+
+player = Tank()
 
 while True:
     # clear the screen every time before drawing new objects
@@ -26,18 +58,10 @@ while True:
             pygame.quit()
             quit()
 
+    rt = (rt + rot_speed) % 360
+    # rt = 0
 
-    rot = (rot + rot_speed) % 360
-
-    r_bild = pygame.transform.rotate(s_bild, 45)
-
-    pos = (rec.center[0]-(r_bild.get_width()/2), rec.center[1])
-    screen.blit(r_bild, pos)
-    pygame.draw.circle(screen, (255, 0, 0), rec.center, 3)
-    pygame.draw.circle(screen, (255, 0, 0), rec.topleft, 3)
-    pygame.draw.circle(screen, (255, 0, 0), rec.topright, 3)
-    pygame.draw.circle(screen, (255, 0, 0), rec.bottomleft, 3)
-    pygame.draw.circle(screen, (255, 0, 0), rec.bottomright, 3)
+    player.draw(rt)
 
     pygame.display.flip()
-    clock.tick(FPS)
+    clock.tick(30)
